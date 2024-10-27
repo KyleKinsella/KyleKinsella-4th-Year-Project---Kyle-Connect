92e45ccc06fc91fa5f11f51f9716cc7ca3ccc10f
KyleKinsella <113980354+KyleKinsella@users.noreply.github.com>
2024-10-27 11:40:07 +0000

    updated my code to extract information out of a html file with python
    I have now coded my project to be able to extract information out of a html file and send that information into my database.

---
+++ Iterationonedev/main.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template, request
+import DBcm
+import mysql.connector
+
+config = {
+    "user": "root",
+    "password": "",
+    "database": "users",
+    "host": "localhost"
+}
+
+
+INSERT = """
+    INSERT INTO communicators
+    (username, email, password)
+    VALUES
+    (%s, %s, %s)
+ """
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('makeAccount.html')
+
+
+@app.route('/submit', methods=['POST'])
+def submitForm():
+    print(request.form)  # Debugging step
+    
+    name = request.form.get('name')
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    data = (name, email, password)
+
+    print(f"name: {name}, email: {email}, password: {password}")
+
+    with DBcm.UseDatabase(config) as db:
+        db.execute(INSERT, data)
+
+
+    return f"form submitted! name: {name}, email: {email}, password: {password}"
+
+if __name__ == '__main__':
+    app.run(debug=True)
