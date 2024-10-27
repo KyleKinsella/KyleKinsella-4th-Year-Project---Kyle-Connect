02d2249a760f05e4c606df39bf94dfb980cc3a84
KyleKinsella <113980354+KyleKinsella@users.noreply.github.com>
2024-10-27 13:17:27 +0000

    Update main.py

--- Iterationonedev/templates/main.py
+++ Iterationonedev/templates/main.py
@@ -43,4 +43,4 @@ def submitForm():
     return f"form submitted! name: {name}, email: {email}, password: {password}"
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)
+    app.run(debug=True, port=5000)
