30545be28561a29ecf4c45e8736f17822c22136e
KyleKinsella <113980354+KyleKinsella@users.noreply.github.com>
2024-10-27 12:13:09 +0000

    work......

--- Iterationonedev/main.py
+++ Iterationonedev/main.py
@@ -21,7 +21,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('makeAccount.html')
 
 
 @app.route('/submit', methods=['POST'])
