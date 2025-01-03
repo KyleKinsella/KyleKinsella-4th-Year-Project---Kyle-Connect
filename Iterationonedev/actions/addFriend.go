754e72912425becceeb3621146e744d6aa878d6c
KyleKinsella <113980354+KyleKinsella@users.noreply.github.com>
2025-01-03 16:01:14 +0000

    started to make add friend / send friend request
    here I have wrote some code to get the username from the html form and then retrieve that name from the database, if this is true then I am able to make / send the friend request (i need to code this out still) , otherwise the name entered is not equal to the name retrieved from the database.

---
+++ Iterationonedev/actions/addFriend.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"html/template"
+	"strings"
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+	"testing/utils"
+)
+
+var addFriend = `
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Kyle Connect - Add a friend</title>
+</head>
+<body>
+    <h1>Add a friend</h1>
+	<p>Enter a name of a friend that you would like to add</p>
+    <form method="POST" action="/form">
+        <label for="username">Username:</label>
+        <input type="username" id="username" name="username" placeholder="Enter username to add" required><br><br>
+
+        <input type="submit" value="Send friend request">
+    </form>
+</body>
+</html>
+`
+
+type User struct {
+    Username string
+	UI template.HTML
+}
+
+// Handler function to serve the form and process submissions
+func formHandler(w http.ResponseWriter, r *http.Request) {
+    // Parse the HTML template
+    tmpl, err := template.New("form").Parse(addFriend)
+    if err != nil {
+        log.Fatal(err)
+    }
+    // Initialize form data
+    userData := User{}
+
+    if r.Method == http.MethodPost {
+        // Parse form data
+        err := r.ParseForm()
+        if err != nil {
+            http.Error(w, "Error parsing form data", http.StatusBadRequest)
+            return
+        }
+
+        userData.Username = r.FormValue("username")
+		enteredUsername := userData.Username
+
+        db, err := sql.Open("mysql", "root@tcp(127.0.0.1)/kyleconnect") // this line of code works for localhost but not docker!
+        // db, err := sql.Open("mysql", "root@tcp(host.docker.internal:3306)/kyleconnect?parseTime=true")
+
+		username, er := utils.RetrieveUsernameFromDb(db, enteredUsername)
+		if err != nil {
+			log.Fatal(er)
+		}
+		
+		if er != nil {
+			log.Fatal("error retrieving username from database", er)
+		}
+		fmt.Println("here is the inputed username retrieved from the database", username, "test", userData.Username)
+
+		if strings.TrimSpace(strings.ToLower(username)) == strings.TrimSpace(strings.ToLower(enteredUsername)) {			
+			fmt.Println("yesss", username)
+			// here i need to make the friend request logic
+		} else {
+			fmt.Println("noooo", er)
+		}
+
+        utils.CatchError(err)
+        defer db.Close()
+    }
+    // Render the HTML template with the form data
+    tmpl.Execute(w, userData)
+}
+
+func main() {
+    // Set up the route and handler for the form
+    http.HandleFunc("/", formHandler)
+
+    // Start the HTTP server
+    fmt.Println("Server started at http://localhost:8082")
+    log.Fatal(http.ListenAndServe(":8082", nil))
+}
